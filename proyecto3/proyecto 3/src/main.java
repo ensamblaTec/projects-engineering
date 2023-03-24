17720ce7fadd3bb235a7ac919ceacaa0bc3d7ec7
gioeh07 <19030157@itcelaya.edu.mx>
2023-03-23 23:17:34 -0600

    proyecto3

---
+++ proyecto3/proyecto 3/src/main.java
@@ -0,0 +1,15 @@
+public class main
+{
+
+    public boolean hasOne(int n)
+    {
+        while(n%10!=0||n==10)
+        {
+            if(n%10 == 1)
+                return true;
+            else
+                n/=10;
+        }
+        return false;
+    }
+}
